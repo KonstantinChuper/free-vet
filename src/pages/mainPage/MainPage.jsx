2cd4e7e74d1af959a7bc0d4d5b194e700be1bd17
Konstantin <konstantyn.chuper@gmail.com>
2024-10-29 07:37:39 +0100

    fixes to main page

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,10 +1,10 @@
-import s from "./mainPage.module.css";
 import MainHeader from "../../components/mainHeader/MainHeader";
 import Opportunity from "../../components/opportunity/Opportunity.jsx";
 import Statistics from "../../components/statistics/Statistics.jsx";
 import TailBook from "../../components/tailBook/TailBook.jsx";
 import Footer from "../../components/footer/Footer.jsx";
 import Container from "../../components/container/Container.jsx";
+import { useEffect } from "react";
 
 const MainPage = () => {
 

43c2e2b9edcb0de8fb97bfc271823fa0a91522de
Eugenia Smith <ev-genius4@mail.ru>
2024-11-07 11:13:00 +0100

    Styling of the answer component is added

--- src/components/vetAnswer/VetAnswer.jsx
+++ src/components/vetAnswer/VetAnswer.jsx
@@ -4,14 +4,16 @@ import { useTranslation } from "react-i18next";
 const VetAnswer = () => {
   const { t } = useTranslation();
 
-  <div className={styles.answer_container}>
-    <h6 className={styles.answer_heading}>{t("userPage.vetAnswer")}</h6>
-    <p className={styles.answer_contents}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam harum,
-      pariatur nemo quasi cupiditate sapiente, eos quibusdam corrupti recusandae
-      officiis eum, possimus necessitatibus deleniti.
-    </p>
-  </div>;
+  return (
+    <div className={styles.answer_container}>
+      <h5 className={styles.answer_heading}>{t("userPage.vetAnswer")}</h5>
+      <p className={styles.answer_contents}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam harum,
+        pariatur nemo quasi cupiditate sapiente, eos quibusdam corrupti
+        recusandae officiis eum, possimus necessitatibus deleniti.
+      </p>
+    </div>
+  );
 };
 
 export default VetAnswer;

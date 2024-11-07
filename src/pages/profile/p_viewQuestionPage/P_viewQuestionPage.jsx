cd3e4985ae1c18f91ecdabc76efa18916dbd7301
Eugenia Smith <ev-genius4@mail.ru>
2024-11-07 14:14:02 +0100

    Some style alterations

--- src/pages/profile/p_viewQuestionPage/P_viewQuestionPage.jsx
+++ src/pages/profile/p_viewQuestionPage/P_viewQuestionPage.jsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import Modal from "../../../components/modalProfile/ModalProfile.jsx";
 import VetAnswer from "../../../components/vetAnswer/VetAnswer.jsx";
 import { useState } from "react";
+import ViewPageHeader from "../../../components/viewPageHeader/ViewPageHeader.jsx";
 
 //в файле использованы тестовые данные, т.к. это необходимо для рендера модалки
 
@@ -22,7 +23,8 @@ const P_viewQuestionPage = () => {
 
   return (
     <div className={s.p_viewQuestionPage}>
-      <h6 className={s.page_heading}>{t("userPage.viewQuestion")}</h6>
+      {/* <h6 className={s.page_heading}>{t("userPage.viewQuestion")}</h6> */}
+      <ViewPageHeader fontSize={36} titleKey={t("userPage.viewQuestion")} />
       <VetAnswer />
       <div>
         {isModalVisible && <Modal linksArr={testLinks} onClose={closeModal} />}

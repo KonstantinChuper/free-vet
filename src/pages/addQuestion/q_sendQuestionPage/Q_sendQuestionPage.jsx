784c903706f744f2d2a6ed0a88a9a09cc67f3547
Sergey-McArrow <sergey.mcarrow@gmail.com>
2024-11-07 22:18:24 +0100

    fix: file send to location add updateQuestion

--- src/pages/addQuestion/q_sendQuestionPage/Q_sendQuestionPage.jsx
+++ src/pages/addQuestion/q_sendQuestionPage/Q_sendQuestionPage.jsx
@@ -1,150 +1,157 @@
-import React from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { useTranslation } from "react-i18next";
-import s from "./q_sendQuestionPage.module.css";
-import FormHeader from "../../../components/formHeader/FormHeader";
-import LineHeader from "../../../components/lineHeader/LineHeader";
-import close from "../../../assets/close.svg";
-import CustomTextarea from "../../../components/customTextarea/CustomTextarea";
-import CustomButtonSubmit from "../../../components/customButtonSubmit/CustomButtonSubmit";
-import { addQuestion } from "../../../utils/api.js";
+import React from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import s from './q_sendQuestionPage.module.css'
+import FormHeader from '../../../components/formHeader/FormHeader'
+import LineHeader from '../../../components/lineHeader/LineHeader'
+import close from '../../../assets/close.svg'
+import CustomTextarea from '../../../components/customTextarea/CustomTextarea'
+import CustomButtonSubmit from '../../../components/customButtonSubmit/CustomButtonSubmit'
+import { updateQuestion } from '../../../utils/api.js'
 
 const Q_sendQuestionPage = () => {
-  const { t } = useTranslation();
-  const location = useLocation();
-  const navigate = useNavigate();
-  const { petArt, petWeight, petGender, isHomeless, files, userId } =
-    location.state || {};
+    const { t } = useTranslation()
+    const location = useLocation()
+    const navigate = useNavigate()
+    const { petArt, petWeight, petGender, isHomeless, files, userId } =
+        location.state || {}
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm({
-    mode: "onChange",
-  });
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isValid },
+    } = useForm({
+        mode: 'onChange',
+    })
 
-  const onSubmit = async (data) => {
-    try {
-      const formData = new FormData();
+    const onSubmit = async (data) => {
+        try {
+            const dataToSend = {
+                user_id: userId,
+                questions: data.question,
+            }
 
-      // Добавляем текстовые поля формы в formData
-      formData.append("question", data.question);
-      formData.append("petArt", petArt);
-      formData.append("petWeight", petWeight);
-      formData.append("petGender", petGender);
-      formData.append("isHomeless", isHomeless);
-      formData.append("userId", userId);
+            // Отправляем запрос через updateQuestion
+            await updateQuestion(JSON.stringify(dataToSend))
 
-      // Добавляем файлы в formData
-      if (files && files.length > 0) {
-        files.forEach((file, index) => {
-          formData.append(`file_${index}`, file);
-        });
-      }
-
-      // Отправляем запрос через addQuestion
-      await addQuestion(formData);
-
-      // После успешной отправки формы перенаправляем пользователя
-      navigate("/main/question/confirm"),
-        {
-          state: {
-            question: data.question,
-            petArt: data.petArt,
-            petWeight: data.petWeight,
-            petGender: data.petGender,
-            isHomeless: isHomeless,
-            files: files,
-            userId: userId,
-          },
-        };
-    } catch (error) {
-      console.error("Ошибка при отправке вопроса", error);
+            // После успешной отправки формы перенаправляем пользователя
+            navigate('/main/question/confirm'),
+                {
+                    state: {
+                        question: data.question,
+                        petArt: data.petArt,
+                        petWeight: data.petWeight,
+                        petGender: data.petGender,
+                        isHomeless: isHomeless,
+                        files: files,
+                        userId: userId,
+                    },
+                }
+        } catch (error) {
+            console.error('Ошибка при отправке вопроса', error)
+        }
     }
-  };
 
-  return (
-    <div className={s.q_sendQuestionPage}>
-      <div className={s.q_sendQuestionPage_header}>
-        <FormHeader
-          path="/main/question/choice"
-          fontSize={36}
-          titleKey={t("questionPage.title")}
-        />
-        <Link to={"/main/question/choice"}>
-          <img className={s.closeBtn} src={close} alt="close" />
-        </Link>
-      </div>
-      <LineHeader middle={"var(--color-main)"} right={"var(--color-main)"} />
-      <p className={s.q_sendQuestionPage_file_p}>
-        {t("sendQuestionPage.addedMedia")}
-      </p>
-      <div className={s.q_sendQuestionPage_fileBox}>
-        {files && files.length > 0 ? (
-          <div className={s.filesContainer}>
-            {files.map((file, index) => (
-              <div key={index} className={s.fileBox}>
-                {file.type.startsWith("image") ? (
-                  <img src={file.data} alt={`uploaded-file-${index}`} />
-                ) : file.type.startsWith("video") ? (
-                  <video controls src={file.data} />
+    return (
+        <div className={s.q_sendQuestionPage}>
+            <div className={s.q_sendQuestionPage_header}>
+                <FormHeader
+                    path="/main/question/choice"
+                    fontSize={36}
+                    titleKey={t('questionPage.title')}
+                />
+                <Link to={'/main/question/choice'}>
+                    <img className={s.closeBtn} src={close} alt="close" />
+                </Link>
+            </div>
+            <LineHeader
+                middle={'var(--color-main)'}
+                right={'var(--color-main)'}
+            />
+            <p className={s.q_sendQuestionPage_file_p}>
+                {t('sendQuestionPage.addedMedia')}
+            </p>
+            <div className={s.q_sendQuestionPage_fileBox}>
+                {files && files.length > 0 ? (
+                    <div className={s.filesContainer}>
+                        {files.map((file, index) => {
+                            const fileUrl = URL.createObjectURL(file.file)
+                            return (
+                                <div key={index} className={s.fileBox}>
+                                    {/* {file.type.startsWith('image') ? (
+                                    <img
+                                        src={file.data}
+                                        alt={`uploaded-file-${index}`}
+                                    />
+                                ) : file.type.startsWith('video') ? (
+                                    <video controls src={file.data} />
+                                ) : (
+                                    <p>
+                                        {t(
+                                            'sendQuestionPage.unsupportedFileFormat'
+                                        )}
+                                    </p>
+                                )} TODO: findout data structure from backend*/}
+                                    <img
+                                        src={fileUrl}
+                                        alt="Uploaded"
+                                        style={{ maxWidth: '100%' }}
+                                    />
+                                </div>
+                            )
+                        })}
+                    </div>
                 ) : (
-                  <p>{t("sendQuestionPage.unsupportedFileFormat")}</p>
+                    <p>{t('sendQuestionPage.noPhotos')}</p>
                 )}
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>{t("sendQuestionPage.noPhotos")}</p>
-        )}
-      </div>
-      <div className={s.q_sendQuestionPage_description}>
-        <p>
-          {t("sendQuestionPage.petArt")}: {petArt}
-        </p>
-        <p>
-          {t("sendQuestionPage.petWeight")}: {petWeight}
-        </p>
-        <p>
-          {t("sendQuestionPage.petGender")}: {petGender}
-        </p>
-        <p style={{ display: "none" }}>
-          {t("sendQuestionPage.homeless")} {isHomeless ? t("yes") : t("no")}
-        </p>
-      </div>
-      <p className={s.q_sendQuestionPage_p}>
-        {t("sendQuestionPage.writeQuestion")}
-      </p>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <CustomTextarea
-          {...register("question", {
-            required: t("sendQuestionPage.requiredField"),
-          })}
-          rows={8}
-          cols={50}
-          placeholder={t("sendQuestionPage.questionPlaceholder")}
-          style={{
-            borderColor: "var(--color-input-bg-grey)",
-            backgroundColor: "var(--color-text-white)",
-            height: "310px",
-          }}
-        />
-        {errors.question && (
-          <p className={s.errorText}>{errors.question.message}</p>
-        )}
+            </div>
+            <div className={s.q_sendQuestionPage_description}>
+                <p>
+                    {t('sendQuestionPage.petArt')}: {petArt}
+                </p>
+                <p>
+                    {t('sendQuestionPage.petWeight')}: {petWeight}
+                </p>
+                <p>
+                    {t('sendQuestionPage.petGender')}: {petGender}
+                </p>
+                <p style={{ display: 'none' }}>
+                    {t('sendQuestionPage.homeless')}{' '}
+                    {isHomeless ? t('yes') : t('no')}
+                </p>
+            </div>
+            <p className={s.q_sendQuestionPage_p}>
+                {t('sendQuestionPage.writeQuestion')}
+            </p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <CustomTextarea
+                    {...register('question', {
+                        required: t('sendQuestionPage.requiredField'),
+                    })}
+                    rows={8}
+                    cols={50}
+                    placeholder={t('sendQuestionPage.questionPlaceholder')}
+                    style={{
+                        borderColor: 'var(--color-input-bg-grey)',
+                        backgroundColor: 'var(--color-text-white)',
+                        height: '310px',
+                    }}
+                />
+                {errors.question && (
+                    <p className={s.errorText}>{errors.question.message}</p>
+                )}
 
-        <div className={s.btnBox}>
-          <CustomButtonSubmit
-            text={t("sendQuestionPage.submitButton")}
-            padding={"16px 99.5px"}
-            disabled={!isValid}
-          />
+                <div className={s.btnBox}>
+                    <CustomButtonSubmit
+                        text={t('sendQuestionPage.submitButton')}
+                        padding={'16px 99.5px'}
+                        disabled={!isValid}
+                    />
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
-};
+    )
+}
 
-export default Q_sendQuestionPage;
+export default Q_sendQuestionPage

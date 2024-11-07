20fa041253c284354e2fc4e5223be7e500bfe240
Eugenia Smith <ev-genius4@mail.ru>
2024-11-07 10:27:38 +0100

    Mark-up of the question view page has been started

--- src/pages/profile/p_viewQuestionPage/P_viewQuestionPage.jsx
+++ src/pages/profile/p_viewQuestionPage/P_viewQuestionPage.jsx
@@ -1,9 +1,34 @@
-import s from './p_viewQuestionPage.module.css'
+import s from "./p_viewQuestionPage.module.css";
+import { useTranslation } from "react-i18next";
+import Modal from "../../../components/modalProfile/ModalProfile.jsx";
+import VetAnswer from "../../../components/vetAnswer/VetAnswer.jsx";
+import { useState } from "react";
+
+//в файле использованы тестовые данные, т.к. это необходимо для рендера модалки
 
 const P_viewQuestionPage = () => {
-  return (
-    <div className={s.p_viewQuestionPage}></div>
-  )
-}
+  const { t } = useTranslation();
 
-export default P_viewQuestionPage
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
+  const testLinks = [
+    { link: "/profile", text: "Add message" }, // добавить в переводы
+    { link: "/main", text: "End session" }, // добавить в переводы
+  ];
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <div className={s.p_viewQuestionPage}>
+      <h6 className={s.page_heading}>{t("userPage.viewQuestion")}</h6>
+      <VetAnswer />
+      <div>
+        {isModalVisible && <Modal linksArr={testLinks} onClose={closeModal} />}
+      </div>
+    </div>
+  );
+};
+
+export default P_viewQuestionPage;

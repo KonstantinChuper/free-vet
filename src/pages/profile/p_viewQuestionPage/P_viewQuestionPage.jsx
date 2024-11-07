5ef40cfede6ef77826f69b624b90bda03290fbb6
Eugenia Smith <ev-genius4@mail.ru>
2024-11-07 20:38:03 +0100

    Question component is added

--- src/pages/profile/p_viewQuestionPage/P_viewQuestionPage.jsx
+++ src/pages/profile/p_viewQuestionPage/P_viewQuestionPage.jsx
@@ -1,36 +1,43 @@
-import s from "./p_viewQuestionPage.module.css";
-import { useTranslation } from "react-i18next";
-import Modal from "../../../components/modalProfile/ModalProfile.jsx";
-import VetAnswer from "../../../components/vetAnswer/VetAnswer.jsx";
-import { useState } from "react";
-import ViewPageHeader from "../../../components/viewPageHeader/ViewPageHeader.jsx";
+import s from './p_viewQuestionPage.module.css'
+import { useTranslation } from 'react-i18next'
+import Modal from '../../../components/modalProfile/ModalProfile.jsx'
+import VetAnswer from '../../../components/vetAnswer/VetAnswer.jsx'
+import { useState } from 'react'
+import ViewPageHeader from '../../../components/viewPageHeader/ViewPageHeader.jsx'
+import { Question } from '../../../components/shared/question/Question.jsx'
 
 //в файле использованы тестовые данные, т.к. это необходимо для рендера модалки
 
 const P_viewQuestionPage = () => {
-  const { t } = useTranslation();
+    const { t } = useTranslation()
 
-  const [isModalVisible, setIsModalVisible] = useState(true);
+    const [isModalVisible, setIsModalVisible] = useState(true)
 
-  const testLinks = [
-    { link: "/profile", text: "Add message" }, // добавить в переводы
-    { link: "/main", text: "End session" }, // добавить в переводы
-  ];
+    const testLinks = [
+        { link: '/profile', text: 'Add message' }, // добавить в переводы
+        { link: '/main', text: 'End session' }, // добавить в переводы
+    ]
 
-  const closeModal = () => {
-    setIsModalVisible(false);
-  };
+    const closeModal = () => {
+        setIsModalVisible(false)
+    }
 
-  return (
-    <div className={s.p_viewQuestionPage}>
-      {/* <h6 className={s.page_heading}>{t("userPage.viewQuestion")}</h6> */}
-      <ViewPageHeader fontSize={36} titleKey={t("userPage.viewQuestion")} />
-      <VetAnswer />
-      <div>
-        {isModalVisible && <Modal linksArr={testLinks} onClose={closeModal} />}
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className={s.p_viewQuestionPage}>
+            {/* <h6 className={s.page_heading}>{t("userPage.viewQuestion")}</h6> */}
+            <ViewPageHeader
+                fontSize={36}
+                titleKey={t('userPage.viewQuestion')}
+            />
+            <Question />
+            <VetAnswer />
+            <div>
+                {isModalVisible && (
+                    <Modal linksArr={testLinks} onClose={closeModal} />
+                )}
+            </div>
+        </div>
+    )
+}
 
-export default P_viewQuestionPage;
+export default P_viewQuestionPage

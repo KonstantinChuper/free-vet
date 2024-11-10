b3045f7145257375d5f7f52e9e6c97d434884b2c
Eugenia Smith <ev-genius4@mail.ru>
2024-11-10 12:24:45 +0100

    Page side padding was changed to 10px for better alignment of the elements

--- src/pages/profile/p_viewQuestionPage/P_viewQuestionPage.jsx
+++ src/pages/profile/p_viewQuestionPage/P_viewQuestionPage.jsx
@@ -1,47 +1,46 @@
-import s from './p_viewQuestionPage.module.css'
-import { useTranslation } from 'react-i18next'
-import Modal from '../../../components/modalProfile/ModalProfile.jsx'
-import VetAnswer from '../../../components/vetAnswer/VetAnswer.jsx'
-import { useState } from 'react'
-import ViewPageHeader from '../../../components/viewPageHeader/ViewPageHeader.jsx'
-import { Question } from '../../../components/shared/question/Question.jsx'
+import s from "./p_viewQuestionPage.module.css";
+import { useTranslation } from "react-i18next";
+import Modal from "../../../components/modalProfile/ModalProfile.jsx";
+import VetAnswer from "../../../components/vetAnswer/VetAnswer.jsx";
+import { useState } from "react";
+import ViewPageHeader from "../../../components/viewPageHeader/ViewPageHeader.jsx";
+import { Question } from "../../../components/shared/question/Question.jsx";
 
 //в файле использованы тестовые данные, т.к. это необходимо для рендера модалки
 
 const P_viewQuestionPage = () => {
-    const { t } = useTranslation()
+  const { t } = useTranslation();
 
-    const [isModalVisible, setIsModalVisible] = useState(false)
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-    const testLinks = [
-        { link: '/profile', text: 'Add message' }, // добавить в переводы
-        { link: '/main', text: 'End session' }, // добавить в переводы
-    ]
+  const testLinks = [
+    { link: "/profile", text: "Add message" }, // добавить в переводы
+    { link: "/main", text: "End session" }, // добавить в переводы
+  ];
 
-    // чтобы открыть модальное окно
-    const openModal = () => {
-        setIsModalVisible(true)
-    }
+  // чтобы открыть модальное окно
+  const openModal = () => {
+    setIsModalVisible(true);
+  };
 
-    const closeModal = () => {
-        setIsModalVisible(false)
-    }
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
 
-    return (
-        <div className={s.p_viewQuestionPage}>
-            <ViewPageHeader
-                fontSize={36}
-                titleKey={t('userPage.viewQuestion')}
-            />
-            <Question openModal={openModal} />
-            <VetAnswer />
-            <div>
-                {isModalVisible && (
-                    <Modal linksArr={testLinks} onClose={closeModal} />
-                )}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={s.p_viewQuestionPage}>
+      <ViewPageHeader
+        path={"/profile/questions"}
+        fontSize={36}
+        titleKey={t("userPage.viewQuestion")}
+      />
+      <Question is_awaited={true} openModal={openModal} />
+      <VetAnswer />
+      <div>
+        {isModalVisible && <Modal linksArr={testLinks} onClose={closeModal} />}
+      </div>
+    </div>
+  );
+};
 
-export default P_viewQuestionPage
+export default P_viewQuestionPage;

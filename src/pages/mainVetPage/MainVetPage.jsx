5c339f2be10ea0c02559fd08c6c7f09081d1378f
Konstantin <konstantyn.chuper@gmail.com>
2024-11-12 16:14:26 +0100

    fix useEffect import

--- src/pages/mainVetPage/MainVetPage.jsx
+++ src/pages/mainVetPage/MainVetPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect } from "react";
 import Container from "../../components/container/Container";
 import MainHeader from "../../components/mainHeader/MainHeader";
 import TailBook from "../../components/tailBook/TailBook";

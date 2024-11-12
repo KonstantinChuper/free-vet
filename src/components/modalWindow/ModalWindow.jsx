b07120c55c011b1809102fbc73ae7c84d8d7a10c
KeyError34 <150452663+KeyError34@users.noreply.github.com>
2024-11-12 22:29:50 +0100

    modifided

--- src/components/modalWindow/ModalWindow.jsx
+++ src/components/modalWindow/ModalWindow.jsx
@@ -1,21 +1,28 @@
 import styles from "./ModalWindow.module.css";
 import modal_right_arrow_icon from "../../assets/modal_right_arrow_icon.svg";
 import { Link } from "react-router-dom";
-
+import { useTranslation } from "react-i18next";
 function ModalWindow() {
+    const { t } = useTranslation();
   return (
     <div className={styles.modal_container}>
       <Link to="#" className={styles.modal_content}>
-        <button className={styles.modal_button}>Ответить</button>
+        <button className={styles.modal_button}>
+          {t("ModalWindow.answer")}
+        </button>
         <img src={modal_right_arrow_icon} alt="Arrow_Icon" />
       </Link>
       <Link to="#" className={styles.modal_content}>
-        <button className={styles.modal_button}>Завершить вопрос</button>
+        <button className={styles.modal_button}>
+          {t("ModalWindow.closeAnswer")}
+        </button>
         <img src={modal_right_arrow_icon} alt="Arrow_Icon" />
       </Link>
 
       <Link to="#" className={styles.modal_bttn}>
-        <button className={styles.modal_back_button}>Назад</button>
+        <button className={styles.modal_back_button}>
+          {t("ModalWindow.bac")}
+        </button>
       </Link>
     </div>
   );
